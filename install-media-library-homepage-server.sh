420e3e34bb270c2fa4458e1a6fef522f40b44f14
David Craddock <contact@davidcraddock.net>
2022-09-12 20:18:59 +0100

    Corrected

--- install-media-library-homepage-server.sh
+++ install-media-library-homepage-server.sh
@@ -5,7 +5,6 @@ set -e
 sudo mkdir -p /var/opt/media-library-homepage-server
 sudo chown "$UID" -R /var/opt/media-library-homepage-server
 git clone git@github.com:wordswords/media-library-homepage.git /var/opt/media-library-homepage-server
-chmod u+x "/var/opt/media-library-homepage-server/*.sh"
 sudo cp /var/opt/media-library-homepage-server/media-library-homepage-server /etc/init.d/
 sudo chmod u+x /etc/init.d/media-library-homepage-server
 
