db6ed70cbf71d01ab5f570ab8f4d7f2ff0cb3624
David Craddock <wordswords@users.noreply.github.com>
2022-09-13 00:31:40 +0100

    Update update-media-library-homepage.sh

--- update-media-library-homepage.sh
+++ update-media-library-homepage.sh
@@ -1,9 +1,8 @@
 #!/bin/bash 
 
 set -e
-USER_ID=${UID}
 
-sudo chown ${USER_ID} -R /var/opt/media-library-homepage-server
+sudo chown ${USER} -R /var/opt/media-library-homepage-server
 cd /var/opt/media-library-homepage-server
 git pull origin main
 sudo chown root -R /var/opt/media-library-homepage-server
