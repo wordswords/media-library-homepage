19e73e0d315219ee35ecf9423ea0e07b0c8be695
David Craddock <contact@davidcraddock.net>
2022-09-12 20:21:06 +0100

    Fixed permissions for init.d script

--- install-media-library-homepage-server.sh
+++ install-media-library-homepage-server.sh
@@ -6,5 +6,4 @@ sudo mkdir -p /var/opt/media-library-homepage-server
 sudo chown "$UID" -R /var/opt/media-library-homepage-server
 git clone git@github.com:wordswords/media-library-homepage.git /var/opt/media-library-homepage-server
 sudo cp /var/opt/media-library-homepage-server/media-library-homepage-server /etc/init.d/
-sudo chmod u+x /etc/init.d/media-library-homepage-server
 
