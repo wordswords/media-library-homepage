c4f131fe2a44bb18f00a9ddc24ca78764ccf7614
David Craddock <contact@davidcraddock.net>
2022-09-12 21:05:18 +0100

    Removed auto-updating because it doesn'the
    work with init.d user

--- start-media-library-homepage-server.sh
+++ start-media-library-homepage-server.sh
@@ -3,8 +3,6 @@
 set -e
 
 cd /var/opt/media-library-homepage-server
-git config --global --add safe.directory /var/opt/media-library-homepage-server || ''
-/usr/bin/git pull origin main
 /usr/bin/python3 -m http.server &>/dev/null &
 
 
