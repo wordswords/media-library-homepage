d58795d34cb59ee32c591cd119c3ee764efa2055
David Craddock <contact@davidcraddock.net>
2022-09-12 20:49:37 +0100

    Fixed startup error

--- start-media-library-homepage-server.sh
+++ start-media-library-homepage-server.sh
@@ -3,8 +3,8 @@
 set -e
 
 cd /var/opt/media-library-homepage-server
-git pull origin main
-/usr/bin/env python3 -m http.server &>/dev/null &
+/usr/bin/git pull origin main
+/usr/bin/python3 -m http.server &>/dev/null &
 
 
 
