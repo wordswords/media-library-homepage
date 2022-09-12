ca9b6d293c0ec1604b9907413d83466f4be34830
David Craddock <contact@davidcraddock.net>
2022-09-12 20:45:51 +0100

    Fixed startup error

--- install-media-library-homepage-server.sh
+++ install-media-library-homepage-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-
 sudo mkdir -p /var/opt/media-library-homepage-server
 sudo chown "$UID" -R /var/opt/media-library-homepage-server
+sudo git config --global --add safe.directory /var/opt/media-library-homepage-server
 git clone git@github.com:wordswords/media-library-homepage.git /var/opt/media-library-homepage-server
 sudo cp /var/opt/media-library-homepage-server/media-library-homepage-server /etc/init.d/
 
