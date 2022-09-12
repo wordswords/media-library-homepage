844d5139fa03480bc3c1a6cd3822648fc2598880
David Craddock <contact@davidcraddock.net>
2022-09-12 20:57:43 +0100

    Fixed startup problem maybe

--- start-media-library-homepage-server.sh
+++ start-media-library-homepage-server.sh
@@ -3,6 +3,7 @@
 set -e
 
 cd /var/opt/media-library-homepage-server
+git config --global --add safe.directory /var/opt/media-library-homepage-server || ''
 /usr/bin/git pull origin main
 /usr/bin/python3 -m http.server &>/dev/null &
 
