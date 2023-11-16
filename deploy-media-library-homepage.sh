7cc4fe8ca1cfd7f56b56338660a716dfcb5a3625
David Craddock <contact@davidcraddock.net>
2023-11-16 21:08:32 +0000

    Fixed problemo

--- deploy-media-library-homepage.sh
+++ deploy-media-library-homepage.sh
@@ -5,7 +5,7 @@ set -e
 TMPDIR=$(mktemp -d)
 git clone git@github.com:wordswords/media-library-homepage.git "${TMPDIR}"
 rm -f "${TMPDIR}"/*.sh
-sudo cp "${TMPDIR}"/* /var/www/html
+sudo cp -r "${TMPDIR}"/* /var/www/html
 rm -rf "${TMPDIR}"
 
 
