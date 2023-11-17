cb373416fa0507f2dc14855b5a1f9ba8315fd12d
David Craddock <contact@davidcraddock.net>
2023-11-17 00:55:21 +0000

    Fixed problems..

--- deploy-media-library-homepage.sh
+++ deploy-media-library-homepage.sh
@@ -6,5 +6,5 @@ TMPDIR=$(mktemp -d)
 git clone git@github.com:wordswords/media-library-homepage.git "${TMPDIR}"
 rm -f "${TMPDIR}"/*.sh
 sudo rsync -avhH --delete --remove-source-files "${TMPDIR}" /var/www/html
-
+sudo rm -rf "${TMPDIR}"
 
