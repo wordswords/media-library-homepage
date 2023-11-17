f4bdf7d89b318d35a3e44007519b19efb8078f2e
David Craddock <contact@davidcraddock.net>
2023-11-17 17:42:20 +0000

    Fixed daily cron media library homepage deploy

--- deploy-media-library-homepage.sh
+++ deploy-media-library-homepage.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -e
+set -x
+
+if [ "$(id -u)" -ne 0 ] ; then
+    echo 'You must be root to run this script'
+    exit 1
+fi
+
 
 TMPDIR=$(mktemp -d)
 git clone git@github.com:wordswords/media-library-homepage.git "${TMPDIR}"
---
+++ remove-and-install-import-cron.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+
+sudo chown root:users ./daily-cron-media-library-homepage-deploy-screen # has to be owned by root to run as cron
+sudo chmod 775 ./daily-cron-media-library-homepage-deploy-screen
+sudo ln -sf ${PWD}/daily-cron-media-library-homepage-deploy-screen /etc/cron.daily/daily-cron-media-library-homepage-deploy-screen
+
+
+
+
+
+
