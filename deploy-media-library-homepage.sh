a322950dff42fecaaf33a899d23256366b4bede7
David Craddock <contact@davidcraddock.net>
2023-11-17 00:39:40 +0000

    Proper move

--- deploy-media-library-homepage.sh
+++ deploy-media-library-homepage.sh
@@ -5,7 +5,6 @@ set -e
 TMPDIR=$(mktemp -d)
 git clone git@github.com:wordswords/media-library-homepage.git "${TMPDIR}"
 rm -f "${TMPDIR}"/*.sh
-sudo cp -r "${TMPDIR}"/* /var/www/html
-rm -rf "${TMPDIR}"
+sudo rsync -avhH --delete --remove-source-files "${TMPDIR}" /var/www/html
 
 
