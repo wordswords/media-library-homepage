70a6493eecf5991993684db04588903262d34945
David Craddock <contact@davidcraddock.net>
2023-11-17 17:55:24 +0000

    Fixed

--- deploy-media-library-homepage.sh
+++ deploy-media-library-homepage.sh
@@ -8,10 +8,8 @@ if [ "$(id -u)" -ne 0 ] ; then
     exit 1
 fi
 
-
-TMPDIR=$(mktemp -d)
-sudo runuser -u david -- /bin/zsh -c "git clone git@github.com:wordswords/media-library-homepage.git ${TMPDIR}/hp"
-sudo rm -f "${TMPDIR}/hp"/*.sh
-sudo rsync -avhH --delete --remove-source-files "${TMPDIR}/hp" /var/www/html
-sudo rm -rf "${TMPDIR}"
-
+sudo runuser -u david -- /bin/zsh -c """ \
+&& TMPDIR=$(mktemp -d) && git clone git@github.com:wordswords/media-library-homepage.git ${TMPDIR}/hp \
+&& sudo rm -f ${TMPDIR}/hp/*.sh \
+&& sudo rsync -avhH --delete --remove-source-files ${TMPDIR}/hp /var/www/html \
+&& sudo rm -rf ${TMPDIR}"""
