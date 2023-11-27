101887dbf547f970456570dcf5c448371298ae7c
David Craddock <contact@davidcraddock.net>
2023-11-27 02:28:43 +0000

    Fix for sudo problems

--- deploy-media-library-homepage.sh
+++ deploy-media-library-homepage.sh
@@ -8,5 +8,4 @@ if [ "$(id -u)" -ne 0 ] ; then
     exit 1
 fi
 
-
-sudo runuser -u david -- /bin/zsh -c 'export tmpdir=$(mktemp -d) && cd $tmpdir && git clone git@github.com:wordswords/media-library-homepage.git ./hp && sudo rm -f ./hp/*.sh  && sudo rsync -avhH --delete --remove-source-files ./hp /var/www/html && sudo rm -rf ./hp'
+sudo export tmpdir=$(mktemp -d) && cd $tmpdir && git clone git@github.com:wordswords/media-library-homepage.git ./hp && sudo rm -f ./hp/*.sh  && sudo rsync -avhH --delete --remove-source-files ./hp /var/www/html && sudo rm -rf ./hp'
