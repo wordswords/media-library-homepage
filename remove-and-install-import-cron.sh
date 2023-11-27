9a9168d59e40deccac6134d0d3016fa9745f5c1f
David Craddock <contact@davidcraddock.net>
2023-11-27 02:28:59 +0000

    Fix for symblink problems

--- remove-and-install-import-cron.sh
+++ remove-and-install-import-cron.sh
@@ -5,7 +5,8 @@ set -e
 
 sudo chown root:users ./daily-cron-media-library-homepage-deploy-screen # has to be owned by root to run as cron
 sudo chmod 775 ./daily-cron-media-library-homepage-deploy-screen
-sudo ln -sf ${PWD}/daily-cron-media-library-homepage-deploy-screen /etc/cron.daily/daily-cron-media-library-homepage-deploy-screen
+sudo rm -f /etc/cron.daily/daily-cron-media-library-homepage-deploy-screen
+sudo cp -f ${PWD}/daily-cron-media-library-homepage-deploy-screen /etc/cron.daily/daily-cron-media-library-homepage-deploy-screen
 
 
 
