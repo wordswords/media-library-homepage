d6c1c71ee8493f1461fafde0d1ad81abc3e44f04
David Craddock <contact@davidcraddock.net>
2023-12-12 22:10:36 +0000

    Should work now.. proper img tag stripping

--- remove-all-image-links-from-playnite-export.sh
+++ remove-all-image-links-from-playnite-export.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-find ./PlayNiteHTMLExport -name '*.html' -exec sed -i 's/\<img.*\>//g' {} \;
-
-
+## Non greedy match hack
+## https://stackoverflow.com/questions/1103149/non-greedy-reluctant-regex-matching-in-sed/46719361#46719361
+find ./PlayNiteHTMLExport -name '*.html' -exec sed -i 's/<img[^>]*//g' {} \;
