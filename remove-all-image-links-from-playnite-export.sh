a9442b7bab76062501bfb9b3eb5e4ddf9283bafd
David Craddock <contact@davidcraddock.net>
2023-12-12 22:31:48 +0000

    Fixed CSS hack

--- remove-all-image-links-from-playnite-export.sh
+++ remove-all-image-links-from-playnite-export.sh
@@ -7,3 +7,4 @@ set -x
 ## https://stackoverflow.com/questions/1103149/non-greedy-reluctant-regex-matching-in-sed/46719361#46719361
 find ./PlayNiteHTMLExport -name '*.html' -exec sed -i 's/<img[^>]*>&gt;//g' {} \;
 find ./PlayNiteHTMLExport -name '*.html' -exec sed -i 's/<img[^>]*>//g' {} \;
+find ./PlayNiteHTMLExport -name '*.html' -exec sed -i 's/270px/20px/g' {} \;
