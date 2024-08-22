824189e51d61c937246590067d6ced2c4df69de5
David Craddock <contact@davidcraddock.net>
2024-08-22 12:52:02 +0100

    Fixed perms for SWAG hosting

--- deploy-media-library-homepage.sh
+++ deploy-media-library-homepage.sh
@@ -8,9 +8,10 @@ if [ "$(id -u)" -ne 0 ] ; then
     exit 1
 fi
 
-rm -rf /var/www/html/*
+rm -rf /var/www/media-library-homepage/*
 runuser david -- ./deploy-media-library-homepage-as-user.sh
 chown -R root:users /var/www/html
-chmod 775 -R /var/www/html
+chmod 775 -R /var/www/media-library-homepage
+
 
 
