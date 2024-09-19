ece8207ed6b79ddb475925f68cf5a6c6444ecbe5
David Craddock <contact@davidcraddock.net>
2024-09-19 19:00:44 +0100

    Generate html indexes instead of text

--- update-plex-inventories.sh
+++ update-plex-inventories.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 
-tree -H '~' /mnt/video > video-inventory.txt
-tree -H '~' /mnt2/TV > TV-inventory.txt
-tree -H '~' /mnt2/music > music-inventory.txt
-tree -H '~' /mnt/comics > comics-inventory.txt
-tree -H '~' /mnt/ebooks > ebooks-inventory.txt
+tree -H '~' /mnt/video > video-inventory.html
+tree -H '~' /mnt2/TV > TV-inventory.html
+tree -H '~' /mnt2/music > music-inventory.html
+tree -H '~' /mnt/comics > comics-inventory.html
+tree -H '~' /mnt/ebooks > ebooks-inventory.html
 
