3fda0e76e20f5536fe5813d2ffd47f54d3a5ac1f
David Craddock <contact@davidcraddock.net>
2024-09-19 18:59:57 +0100

    Use html output

--- update-plex-inventories.sh
+++ update-plex-inventories.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 
-tree /mnt/video > video-inventory.txt
-tree /mnt2/TV > TV-inventory.txt
-tree /mnt2/music > music-inventory.txt
-tree /mnt/comics > comics-inventory.txt
-tree /mnt/ebooks > ebooks-inventory.txt
+tree -H '~' /mnt/video > video-inventory.txt
+tree -H '~' /mnt2/TV > TV-inventory.txt
+tree -H '~' /mnt2/music > music-inventory.txt
+tree -H '~' /mnt/comics > comics-inventory.txt
+tree -H '~' /mnt/ebooks > ebooks-inventory.txt
 
