b3680d32a07c5afdba2023dd8c1914d3015afd68
David Craddock <contact@davidcraddock.net>
2024-09-23 05:56:23 +0100

    Remove links

--- update-plex-inventories.sh
+++ update-plex-inventories.sh
@@ -3,19 +3,20 @@
 set -e
 set -x
 
-function remove_links() {
-    sed -E 's|<a[^<>]+file:[^>]+>([^<]+)</a>|\1|g' "$1"
+function remove_links () {
+    sed -e 's/<a[^>]*>//g' "$1"
+    sed -e 's/<\/a>//g' "$1"
 }
 
 tree -H '~' /mnt/video > video-inventory.html
-remove_links ./video-inventory.html
+remove_links "./video-inventory.html"
 tree -H '~' /mnt2/TV > TV-inventory.html
-remove_links ./TV-inventory.html
+remove_links "./TV-inventory.html"
 tree -H '~' /mnt2/music > music-inventory.html
-remove_links ./music-inventory.html
+remove_links "./music-inventory.html"
 tree -H '~' /mnt/comics > comics-inventory.html
-remove_links ./comics-inventory.html
+remove_links "./comics-inventory.html"
 tree -H '~' /mnt/ebooks > ebooks-inventory.html
-remove_links ./ebooks-inventory.html
+remove_links "./ebooks-inventory.html"
 
 
