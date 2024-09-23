d761ec054263d90ca29b7eefac05ad821f56328c
David Craddock <contact@davidcraddock.net>
2024-09-23 05:32:30 +0100

    Update plex inventories

--- update-plex-inventories.sh
+++ update-plex-inventories.sh
@@ -3,9 +3,19 @@
 set -e
 set -x
 
+function remove_links() {
+    sed -E 's|<a[^<>]+file:[^>]+>([^<]+)</a>|\1|g' "$1"
+}
+
 tree -H '~' /mnt/video > video-inventory.html
+remove_links ./video-inventory.html
 tree -H '~' /mnt2/TV > TV-inventory.html
+remove_links ./TV-inventory.html
 tree -H '~' /mnt2/music > music-inventory.html
+remove_links ./music-inventory.html
 tree -H '~' /mnt/comics > comics-inventory.html
+remove_links ./comics-inventory.html
 tree -H '~' /mnt/ebooks > ebooks-inventory.html
+remove_links ./ebooks-inventory.html
+
 
