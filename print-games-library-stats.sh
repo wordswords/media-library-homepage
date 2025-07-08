595827f7c87c706239df49b344aae015ab927d96
David Craddock <contact@davidcraddock.net>
2025-07-08 01:45:54 +0100

    Add nsp and xci

--- print-games-library-stats.sh
+++ print-games-library-stats.sh
@@ -8,7 +8,9 @@ bincount=$(find -L /mnt/games -type f -iname '*.bin' | wc -l)
 chdcount=$(find -L /mnt/games -type f -iname '*.chd' | wc -l)
 zipcount=$(find -L /mnt/games -type f -iname '*.zip' | wc -l)
 execount=$(find -L /mnt/games -type f -iname '*.exe' | wc -l)
-gamescount=$(($isocount + $bincount + $chdcount + $zipcount + $execount))
+xcicount=$(find -L /mnt/games -type f -iname '*.xci' | wc -l)
+nspcount=$(find -L /mnt/games -type f -iname '*.nsp' | wc -l)
+gamescount=$(($isocount + $bincount + $chdcount + $zipcount + $execount + $nspcount))
 
 echo "** Games Library Stats"
 echo -e
@@ -18,6 +20,7 @@ echo ".BIN count: $bincount"
 echo ".CHD count: $chdcount"
 echo ".ZIP count: $zipcount"
 echo ".EXE count: $execount"
+echo ".XCI count: $xcicount"
 echo -e
 echo "Total identified possible games: $gamescount"
 echo -e
